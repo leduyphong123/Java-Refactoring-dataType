d6dae715d8647f212b672c4ea24fbdc2879a5df9
leduyphong123 <leduyphongck789@gmail.com>
2023-05-16 15:25:52 +0700

    finnish

---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,44 @@
+public class FizzBuzzTest {
+    void testFizz3() {
+        int number = 3;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+
+    void testFizz6() {
+        int number = 6;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+
+    void testBuzz5() {
+        int number = 5;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+
+    void testBuzz10() {
+        int number = 10;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+
+    void testFizzBuzz15() {
+        int number = 15;
+        String expected = "FizzBuzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+
+    }
+
+    void testFizzBuzz30() {
+        int number = 30;
+        String expected = "FizzBuzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+    }
+}
